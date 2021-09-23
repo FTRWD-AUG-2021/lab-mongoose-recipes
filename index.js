540056d0da658302087054f75c923429a9fc2b10
sarebro <sarebro77@gmail.com>
2021-09-23 15:40:02 -0500

    iteration 2

--- index.js
+++ index.js
@@ -9,19 +9,34 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+	.connect(MONGODB_URI, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then((self) => {
+		console.log(`Connected to the database: "${self.connection.name}"`);
+		// Before adding any recipes to the database, let's remove all existing ones
+		return Recipe.deleteMany();
+	})
+	.then(async () => {
+		await Recipe.create({
+			title: 'Tiramisu',
+			level: 'Easy Peasy',
+			ingredients: [ 'Mascarpone', 'Chocolate', 'Cream', 'Eggs' ],
+			cuisine: 'Italian',
+			dishType: 'dessert',
+			image: 'https://www.askchefdennis.com/wp-content/uploads/2011/04/Tiramisu10b.jpg',
+			duration: 10,
+			creator: 'Carminantonio Iannaccone',
+			created: new Date('12/24/1969')
+		});
+
+		await Recipe.insertMany(data);
+
+		let res = await Recipe.find({ title: 'Tiramisu' });
+		console.log(res[0].title);
+	})
+	.catch((error) => {
+		console.error('Error connecting to the database', error);
+	});
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // Iteration 1
-  title: String,
-  level: { type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
-  ingredients: [ String ],
-  cuisine: String,
-  dishType: { type: String, enum: ["Breakfast", "Main Course", "Soup", "Snack", "Drink", "Dessert", "Other"] },
-  image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
-  duration: { type: Number, min: 0 },
-  creator: String,
-  created: { type: Date, default: Date.now }
+	// Iteration 1
+	title: String,
+	level: { type: String, enum: [ 'Easy Peasy', 'Amateur Chef', 'UltraPro Chef' ] },
+	ingredients: [ String ],
+	cuisine: String,
+	dishType: { type: String, enum: [ 'breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other' ] },
+	image: { type: String, default: 'https://images.media-allrecipes.com/images/75131.jpg' },
+	duration: { type: Number, min: 0 },
+	creator: String,
+	created: { type: Date, default: Date.now }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
